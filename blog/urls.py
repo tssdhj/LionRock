30cd6ed4450229b784f04c3299f6d5f75ff2dc8a
lcl <842955942@qq.com>
2019-02-15 13:47:05 +0800

    修改blog/url.py

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-	path('', views.index, name = 'index'),
+	#path('', views.index, name = 'index'),
+	path('', views.IndexView.as_view(), name='index'),
 	path('post/<int:pk>/', views.detail, name = 'detail'),
 	path('archives/<int:year>/<int:month>/', views.archives, name = 'archives'),
 	path('category/<int:pk>/', views.category, name = 'category'),
